ed6dbdb46ffef966fa27cbe199e91ac8da56c3fa
Ryan Teles <ryanteles2018@gmail.com>
2026-06-16 12:57:26 -0300

    feat: adiciona ItemPedidoService

---
+++ src/main/java/com/ryanteles/sistemas_vendas/service/ItemPedidoService.java
@@ -0,0 +1,52 @@
+package com.ryanteles.sistemas_vendas.service;
+
+import com.ryanteles.sistemas_vendas.entity.ItemPedido;
+import com.ryanteles.sistemas_vendas.repository.ItemPedidoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ItemPedidoService {
+
+    private final ItemPedidoRepository itemPedidoRepository;
+
+    public ItemPedidoService(ItemPedidoRepository itemPedidoRepository) {
+        this.itemPedidoRepository = itemPedidoRepository;
+    }
+
+    public List<ItemPedido> listar(){
+        return itemPedidoRepository.findAll();
+    }
+
+    public Optional<ItemPedido> buscarPorId (Long id){
+        return itemPedidoRepository.findById(id);
+    }
+
+    public ItemPedido salvar (ItemPedido itemPedido){
+         return itemPedidoRepository.save(itemPedido);
+    }
+
+    public void deletar (Long id){
+        Optional<ItemPedido> itemPedido = itemPedidoRepository.findById(id);
+        if(itemPedido.isPresent()){
+            itemPedidoRepository.deleteById(id);
+        }
+    }
+
+    public ItemPedido atualizar (Long id, ItemPedido itemPedidoAtualizado){
+        Optional<ItemPedido> itemPedido = itemPedidoRepository.findById(id);
+        if(itemPedido.isPresent()){
+            ItemPedido itemExistente = itemPedido.get();
+            itemExistente.setQuantidade(itemPedidoAtualizado.getQuantidade());
+            itemExistente.setPrecoUnitario(itemPedidoAtualizado.getPrecoUnitario());
+            itemExistente.setProduto(itemPedidoAtualizado.getProduto());
+            itemExistente.setPedido(itemPedidoAtualizado.getPedido());
+            return itemPedidoRepository.save(itemExistente);
+
+        }
+        return null;
+    }
+
+}
